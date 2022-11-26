6c3e009291b210e481f7edb06c2a9cc7a29ba8ca
Proxym0 <vladi.shapovalov@gmail.com>
2022-11-26 15:05:32 +0300

    save part1/10

---
+++ src/group2/Class2.java
@@ -0,0 +1,10 @@
+package group2;
+
+public class Class2 {
+    public static void main(String[] args) {
+        System.out.print("вывод чисел в обратном порядке:");
+        for(int i=5;i>0;i--){
+            System.out.print(" "+i+" ");
+        }
+    }
+}
