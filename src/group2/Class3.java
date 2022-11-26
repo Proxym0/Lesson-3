a173b1e859400fe99589fc8d526e8a479785580a
Proxym0 <vladi.shapovalov@gmail.com>
2022-11-26 15:09:26 +0300

    save part1/11

---
+++ src/group2/Class3.java
@@ -0,0 +1,12 @@
+package group2;
+
+import java.util.Scanner;
+
+public class Class3 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int a=scanner.nextInt();
+        int sum=1+a;
+        System.out.println(sum);
+    }
+}
