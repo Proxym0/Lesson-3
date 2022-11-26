e2e1a8208745e1b78019049626f682f0a4141ecb
Proxym0 <vladi.shapovalov@gmail.com>
2022-11-26 16:27:09 +0300

    save part1/14

--- src/group2/Class3.java
+++ src/group2/Class3.java
@@ -6,7 +6,14 @@ public class Class3 {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         int a=scanner.nextInt();
-        int sum=1+a;
-        System.out.println(sum);
+        int sum=0;
+        if(a>0){
+        for(int i=0;i<=a;i++) {
+            sum=sum+i;
+        }
+            System.out.println(sum);
+         }else {
+            System.out.println("введите положительно число");
+        }
     }
 }
