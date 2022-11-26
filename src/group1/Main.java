1731a3783be741024a2ca09437c2d7b57c1b4ecd
Proxym0 <vladi.shapovalov@gmail.com>
2022-11-26 13:18:58 +0300

    save part1/4

--- src/group1/Main.java
+++ src/group1/Main.java
@@ -5,45 +5,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введи номер месяца");
-        int month = scanner.nextInt();
+        System.out.println("Введи месяц");
+        String month = scanner.nextLine();
         switch (month) {
-            case 1:
-                System.out.println("January");
+            case "декабрь","январь","февраль":
+                System.out.println("Зима");
                 break;
-            case 2:
-                System.out.println("February");
+            case "март","апрель","май":
+                System.out.println("Весна");
                 break;
-            case 3:
-                System.out.println("March");
+            case "июнь","июль","август":
+                System.out.println("лето");
                 break;
-            case 4:
-                System.out.println("April");
+            case "сентябрь","октябрь","ноябрь":
+                System.out.println("Осень");
                 break;
-            case 5:
-                System.out.println("May");
-                break;
-            case 6:
-                System.out.println("June");
-                break;
-            case 7:
-                System.out.println("July");
-                break;
-            case 8:
-                System.out.println("August");
-                break;
-            case 9:
-                System.out.println("September");
-                break;
-            case 10:
-                System.out.println("October");
-                break;
-            case 11:
-                System.out.println("November");
-                break;
-            case 12:
-                System.out.println("December");
-                break;
+
 
         }
     }
