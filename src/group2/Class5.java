0caf6713b850e6a0d55b7ac18ab98f9e35987b5f
Proxym0 <vladi.shapovalov@gmail.com>
2022-11-26 15:42:53 +0300

    save part1/13

---
+++ src/group2/Class5.java
@@ -0,0 +1,10 @@
+package group2;
+
+public class Class5 {
+    public static void main(String[] args) {
+
+        for(int i=0;i<10;i++){
+
+        }
+    }
+}
---
+++ src/group2/Class6.java
@@ -0,0 +1,9 @@
+package group2;
+
+public class Class6 {
+    public static void main(String[] args) {
+        for(int j=10;j<21;j++){
+            System.out.print(" "+j*j+" ");
+            }
+        }
+    }
