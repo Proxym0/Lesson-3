28c4f8b96d0e73735f7a6c90ef447a16229e71b7
Proxym0 <vladi.shapovalov@gmail.com>
2022-11-26 17:38:17 +0300

    save part1/16

--- src/group2/Class5.java
+++ src/group2/Class5.java
@@ -2,9 +2,14 @@ package group2;
 
 public class Class5 {
     public static void main(String[] args) {
-
-        for(int i=0;i<10;i++){
-
+        int n1 = -5;
+        int n2=0;
+        System.out.print(n2+" ");
+        for(int i = 1; i < 10; i++){
+            n2=n2+n1;
+            System.out.print(n2+" ");
         }
+            System.out.println();
     }
 }
+
