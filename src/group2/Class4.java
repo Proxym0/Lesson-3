d9a9f25c7137e3bcec3a0e17096ab9241aba9ea2
Proxym0 <vladi.shapovalov@gmail.com>
2022-11-26 15:15:15 +0300

    save part1/12

---
+++ src/group2/Class4.java
@@ -0,0 +1,13 @@
+package group2;
+
+public class Class4 {
+    public static void main(String[] args) {
+        int a=0;
+        while (a<100){
+            a++;
+            if(a%7==0){
+                System.out.print(" "+a+" ");
+            }
+        }
+    }
+}
