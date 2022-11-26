7c2a1cb10f194fdb61100252d0525dfa2517b3e6
Proxym0 <vladi.shapovalov@gmail.com>
2022-11-26 16:46:43 +0300

    save part1/15

--- src/group2/Class3.java
+++ src/group2/Class3.java
@@ -4,16 +4,20 @@ import java.util.Scanner;
 
 public class Class3 {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        int a=scanner.nextInt();
-        int sum=0;
-        if(a>0){
-        for(int i=0;i<=a;i++) {
-            sum=sum+i;
-        }
-            System.out.println(sum);
-         }else {
-            System.out.println("введите положительно число");
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.hasNextInt()) {
+            int a = scanner.nextInt();
+            int sum = 0;
+            if (a > 0) {
+                for (int i = 0; i <= a; i++) {
+                    sum = sum + i;
+                }
+                System.out.println(sum);
+            } else {
+                System.out.println("введите положительно число");
+            }
+        } else {
+            System.out.println("введите число");
         }
     }
 }
