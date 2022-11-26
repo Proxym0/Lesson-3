acc58b5df57f5e0137ae76bc2d1a14cbbe9f5b1e
Proxym0 <vladi.shapovalov@gmail.com>
2022-11-26 13:56:22 +0300

    save part1/5

---
+++ src/group1/Class3.java
@@ -0,0 +1,16 @@
+package group1;
+
+import java.util.Scanner;
+
+public class Class3 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int a=scanner.nextInt();
+        if(a%2==0){
+            System.out.println("четное");}
+        else{
+            System.out.println("нечетное");
+
+        }
+    }
+}
