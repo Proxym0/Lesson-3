6e4d6beebfb23d73eafc51b53ee97fa442351f8d
Proxym0 <vladi.shapovalov@gmail.com>
2022-11-26 11:43:30 +0300

    save part1/2

---
+++ src/group3/lesson.java
@@ -0,0 +1,17 @@
+package group3;
+
+public class lesson {
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i < 11; ++i) {
+            for(int j = 1; j < 11; ++j) {
+                System.out.print(i*j+" ");
+            }System.out.println(" ");
+        }
+    }
+}
+
+
+
+
